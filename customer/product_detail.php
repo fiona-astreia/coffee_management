d6afe251c2ad049a936d9ffbdc147334c6fdd251
Trinh Minh Tam <114578738+fiona-astreia@users.noreply.github.com>
2025-12-15 12:01:04 +0700

    Update product_detail.php
    Co-Authored-By: Ngoc Gemmie <22070444@vnu.edu.vn>

--- customer/product_detail.php
+++ customer/product_detail.php
@@ -2,3 +2,20 @@
 session_start();
 require_once '../config/db.php';
 
+// AUTO LOGIN 
+if (!isset($_SESSION['username']) && isset($_COOKIE['remember_me'])) {
+    $token = $_COOKIE['remember_me'];
+    $stmt_find = mysqli_prepare($con, 
+        "SELECT users.* FROM auth_tokens 
+         JOIN users ON auth_tokens.user_id = users.id 
+         WHERE auth_tokens.token = ? AND auth_tokens.expires_at > NOW()");
+    mysqli_stmt_bind_param($stmt_find, "s", $token);
+    mysqli_stmt_execute($stmt_find);
+    $result_find = mysqli_stmt_get_result($stmt_find);
+    if ($user = mysqli_fetch_assoc($result_find)) {
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['role'] = $user['role'];
+    }
+    mysqli_stmt_close($stmt_find);
+}
