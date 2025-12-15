d4784d1ed0cb07338a58d80da11c791c84efce77
Trinh Minh Tam <114578738+fiona-astreia@users.noreply.github.com>
2025-12-15 09:58:45 +0700

    Update home.php

--- home.php
+++ home.php
@@ -63,8 +63,19 @@ $result = mysqli_query($con, $query); // Object - False
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
 
+    <style>
+        body {
+            background-color: #f8f9fa;
+            font-family: 'Poppins', sans-serif;
+            color: #555;
+        }
+
+        
+    </style>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
