99b5fc7ee137f7ede6f94291a595112f578a2c9d
Ngoc Gemmie <22070444@vnu.edu.vn>
2025-12-15 14:49:23 +0700

    Clean up logout function by removing redundant code
    Removed unnecessary closing statements for prepared statement and connection.

--- auth/logout.php
+++ auth/logout.php
@@ -11,8 +11,7 @@ if (isset($_COOKIE['remember_me'])) {
     mysqli_stmt_bind_param($stmt_delete, "s", $token);
     mysqli_stmt_execute($stmt_delete);
     
-    mysqli_stmt_close($stmt_delete);
-    mysqli_close($con);
+   
 }
 
 // 2. Hủy cookie "remember_me" khỏi trình duyệt
