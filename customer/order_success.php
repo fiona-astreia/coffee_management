c8e79398455f2caa93192f553026078fad73eb79
Trinh Minh Tam <114578738+fiona-astreia@users.noreply.github.com>
2025-12-15 11:55:22 +0700

    Update order_success.php
    Co-Authored-By: NhatS8 <198859205+NhatS8@users.noreply.github.com>

--- customer/order_success.php
+++ customer/order_success.php
@@ -3,3 +3,28 @@ session_start();
 // Auto login check nếu cần (tùy chọn)
 $order_id = $_GET['orderid'] ?? 0;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Order Success</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style>
+        body {
+            background: #f8f9fa;
+        }
+
+        
+    </style>
+</head>
+
+<body>
+    <div class="container">
+
+        
+    </div>
+</body>
+
+</html>
