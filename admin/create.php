57c07cce5d60eb9da98d69504ac7ed775c2bb01f
Trinh Minh Tam <114578738+fiona-astreia@users.noreply.github.com>
2025-12-15 10:30:29 +0700

    Update create.php
    Co-Authored-By: ThieuHoai <182103236+ThieuHoai@users.noreply.github.com>

--- admin/create.php
+++ admin/create.php
@@ -102,11 +102,19 @@ mysqli_close($con);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add New Product</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style>
+        .container {
+            margin-top: 30px;
+            max-width: 600px;
+        }
+    </style>
 </head>
 <body>
     
