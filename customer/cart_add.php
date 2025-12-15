daf5841680a325e3ab521bb6616a25f95f4791fc
Trinh Minh Tam <114578738+fiona-astreia@users.noreply.github.com>
2025-12-15 11:33:49 +0700

    Update cart_add.php

--- customer/cart_add.php
+++ customer/cart_add.php
@@ -10,8 +10,8 @@ if (!isset($_SESSION['username'])) {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // 1. Lấy dữ liệu từ form
-    $id = (int) $_POST['product_id'];
-    $quantity = (int) $_POST['quantity'];
+    $id = (int)$_POST['product_id'];
+    $quantity = (int)$_POST['quantity'];
 
     // 2. Validate
     if ($id <= 0 || $quantity <= 0) {
@@ -19,18 +19,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // 3. Lấy thông tin sản phẩm chuẩn từ DB (Để tránh hack giá từ HTML)
-
+    $stmt = mysqli_prepare($con, "SELECT * FROM products WHERE id = ?");
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $product = mysqli_fetch_assoc($result);
 
     if ($product) {
         // 4. Logic Giỏ hàng (Session)
+        
+        
 
+        
 
-
-
-
-
+        
     } else {
-
+        
     }
 }
 ?>
